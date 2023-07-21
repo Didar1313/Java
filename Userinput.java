cce8dc38bcbe956560180bc0f3fe99ee5ca4265e
IB Didar <73778140+Didar1313@users.noreply.github.com>
2023-07-22 01:40:34 +0600

    User_Input

---
+++ Userinput.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Userinput {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int a=scanner.nextInt();
+        System.out.println(a);
+    }
+    
+}
